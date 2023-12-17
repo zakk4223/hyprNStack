70a54e12897a86ddf422b83f0c73d0320bd6ce31
Zakk <zakk@rsdio.com>
2023-12-17 16:25:37 -0500

    Chase hyprland-git

--- nstackLayout.cpp
+++ nstackLayout.cpp
@@ -1405,7 +1405,7 @@ void CHyprNstackLayout::replaceWindowDataWith(CWindow* from, CWindow* to) {
 
 void CHyprNstackLayout::onEnable() {
     for (auto& w : g_pCompositor->m_vWindows) {
-        if (w->m_bIsFloating || !w->m_bMappedX11 || !w->m_bIsMapped || w->isHidden())
+        if (w->m_bIsFloating || !w->m_bIsMapped || w->isHidden())
             continue;
 
         onWindowCreatedTiling(w.get());
