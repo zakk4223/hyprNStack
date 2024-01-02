c44ff8c38149acd7deff9fd9dac65e6b23a2cafb
Zakk <zakk@rsdio.com>
2024-01-02 15:05:32 -0500

    changes from hyprland layouts

--- nstackLayout.cpp
+++ nstackLayout.cpp
@@ -869,6 +869,8 @@ void CHyprNstackLayout::fullscreenRequestForWindow(CWindow* pWindow, eFullscreen
     pWindow->m_bIsFullscreen           = on;
     PWORKSPACE->m_bHasFullscreenWindow = !PWORKSPACE->m_bHasFullscreenWindow;
 
+		pWindow->updateDynamicRules();
+		pWindow->updateWindowDecos();
     g_pEventManager->postEvent(SHyprIPCEvent{"fullscreen", std::to_string((int)on)});
     EMIT_HOOK_EVENT("fullscreen", pWindow);
 
